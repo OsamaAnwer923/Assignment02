64b01834de8985eda1ebb644c234beabae449926
OsamaAnwer923 <osama.anwer923@gmail.com>
2024-12-10 20:36:35 +0500

    Question 1,2 and 3

---
+++ Assignment02Q1.dart
@@ -0,0 +1,4 @@
+void main(){
+  List names = ["osama", "salman", "ahtesham"];
+  print(names);
+}
---
+++ Assignment02Q3.dart
@@ -0,0 +1,17 @@
+void main(){
+  List days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+}
